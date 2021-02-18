79bfeac3cf295859773c4b483cad67ed3ee2c83a
Sebastian Mazurek <smazurek.sm@gmail.com>
2021-02-18 13:58:11 +0100

    added date and main file

---
+++ main.py
@@ -0,0 +1,10 @@
+#%%
+import sklearn
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %%
+df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+df
+# %%
